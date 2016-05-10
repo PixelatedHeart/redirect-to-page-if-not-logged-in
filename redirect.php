39dfed3f269b9af88437b84973a294c90a20db29
Rafa Poveda <raven@raven.es>
2016-05-10 02:22:41 +0200

    Add changes

--- redirect.php
+++ redirect.php
@@ -1,28 +1,35 @@
 <?php
 /**
- * @package Redirect to login if not logged in
- * @version 1.6.3
+ * @package Redirect to a concrete page if the users is not logged in
+ * @version 1.0
  */
 /*
-Plugin Name:   Redirect to login if not logged in
-Plugin URI:    http://wordpress.org/plugins/redirect-to-login-if-not-logged-in/
-Description:   Redirects a user to the login page if the user is not logged in. After login the user gets redirected to the original entry page.
-Author:        Daan Kortenbach
-Version:       1.6.3
-Author URI:    http://daan.kortenba.ch/
-License:       GPL-2.0 or later
-License URI:   http://www.gnu.org/licenses/gpl-2.0.txt
-*/
+Plugin Name:   Redirect to a concrete page if the users is not logged in
+Plugin URI:    http://wordpress.org/plugins/redirect-to-page-if-not-logged-in/
+Description:   Redirects to a concrete page if the user is not logged in.
+Author:        Rafa Poveda
+Version:       1.0
+Author URI:    http://raven.es/
+License:       GPL-3.0 or later
+License URI:   http://www.gnu.org/licenses/gpl-3.0.txt
+ */
 
-add_action( 'template_redirect', function() {
+add_action( 'template_redirect', 'bi0_redirect_page' );
 
-  if ( is_user_logged_in() ) return;
+function bi0_redirect_page() {
 
-  $allowed_pages = array( 7966 ); // 7966 is login page
+    if ( is_user_logged_in() ) {
+        return;
+    }
 
-  if ( ! in_array( get_queried_object_id(), $allowed_pages ) ) {
-    wp_redirect( site_url( '/login/' ) ); 
-    exit();
-  }
+    // TODO add the allowed pages via admin menu
+    $allowed_pages = array( 7966 );
+// 7966 is login page
 
-});
+    if ( !in_array( get_queried_object_id(), $allowed_pages ) ) {
+        // TODO add the site url via admin menu
+        wp_redirect( site_url( '/login/' ) );
+        exit();
+    }
+
+}
